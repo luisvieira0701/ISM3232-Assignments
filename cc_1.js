4dea3cb64574ac0a2a89d5c91033d54862d86957
Luis Vieira <luisvieira@usf.edu>
2024-09-01 14:52:46 -0400

    Task 1 - Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,4 @@
-
+let employeeName = "Michael Jordan" 
+const employeeID = 23
+var isActive = true 
+console.log(employeeName, typeof "Michael Jordan", employeeID, typeof 23, isActive, typeof true) 
