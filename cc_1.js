df43c6eff9bc4204c82ae7a858acf4de72a7128f
LuisVieira0701 <luisvieira@usf.edu>
2024-09-01 20:25:11 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -3,4 +3,12 @@ const employeeID = 23
 var isActice = true
 console.log(employeeName, typeof "Michael Jordan", employeeID, typeof 23, isActice, typeof true)
 
+let products = ["Ice Cream", "Popsicle", "Gelato"];
+const productDetails = {
+  name: "Gelato",
+  price: 500,
+  inStock: true
+};
+console.log(products);
+console.log(productDetails);
 
